8d83c7fa6eb11d65cc378df5641bafbe5ca3a110
SARDONYX-sard <68905624+SARDONYX-sard@users.noreply.github.com>
2022-08-26 06:23:36 +0000

    refactor(os): implement `panic` macro with `print`

--- os/src/lang_items.rs
+++ os/src/lang_items.rs
@@ -1,6 +1,27 @@
 use core::panic::PanicInfo;
 
+use crate::{println, sbi::shutdown};
+
+/// Prints to the standard output, with a newline
+/// and shutdown.
+///
+/// # Examples
+///
+/// ```
+/// panic!("Shutdown machine!");
+/// // >Panicked at src/main.rs:16 Shutdown machine!
+/// ```
 #[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
-    loop {}
+fn panic(info: &PanicInfo) -> ! {
+    if let Some(location) = info.location() {
+        println!(
+            "Panicked at {}:{} {}",
+            location.file(),
+            location.line(),
+            info.message().unwrap()
+        );
+    } else {
+        println!("Panicked: {}", info.message().unwrap())
+    }
+    shutdown()
 }
