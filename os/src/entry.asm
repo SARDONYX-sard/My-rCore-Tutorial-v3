9f7719332e5131588698878756d5a8c23b83b5f9
SARDONYX-sard <68905624+SARDONYX-sard@users.noreply.github.com>
2022-08-26 04:04:03 +0000

    refactor(os): modify to be able to init the stack
    in entry

--- os/src/entry.asm
+++ os/src/entry.asm
@@ -3,4 +3,12 @@
 .section .text.entry
 .globl _start
 _start:
-  li x1, 100
+  la sp, boot_stack_top # load address dist, src_symbol: sp <- boot_stack_top
+  call rust_main        # This function is defined in main.rs
+
+  .section .bss.stack
+  .globl boot_stack     # become global symbol. base stack
+boot_stack:
+  .space 4096 * 16      # 64KiB
+  .globl boot_stack_top # become global symbol to label
+boot_stack_top:
