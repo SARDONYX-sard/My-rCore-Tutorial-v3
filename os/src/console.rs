4c198a0e325c9854c58ff8ab4fe143b74a8fc29a
SARDONYX-sard <68905624+SARDONYX-sard@users.noreply.github.com>
2022-08-26 06:22:46 +0000

    feat(os): add `print` macro

---
+++ os/src/console.rs
@@ -0,0 +1,66 @@
+use core::fmt::{self, Write};
+
+use crate::sbi::console_putchar;
+
+struct Stdout;
+
+impl Write for Stdout {
+    fn write_str(&mut self, s: &str) -> core::fmt::Result {
+        for c in s.chars() {
+            console_putchar(c as usize)
+        }
+        Ok(())
+    }
+}
+
+pub fn print(args: fmt::Arguments) {
+    Stdout.write_fmt(args).unwrap();
+}
+
+/// Prints to the standard output.
+///
+/// Equivalent to the [`println!`] macro except that a newline is not printed at
+/// the end of the message.
+///
+/// Use `print!` only for the primary output of your program.
+///
+/// [`println!`]: crate::println
+///
+/// # Examples
+///
+/// ```
+/// print!("this ");
+/// print!("will ");
+/// print!("be ");
+/// print!("on ");
+/// print!("the ");
+/// print!("same ");
+/// print!("line ");
+///
+/// print!("this string has a newline, why not choose println! instead?\n");
+/// ```
+#[macro_export]
+macro_rules! print {
+    ($fmt: literal $(, $(arg: tt)+)?) => {
+        $crate::console::print(format_args!($fmt $(,$(arg)+)?));
+    };
+}
+
+/// Prints to the standard output, with a newline.
+///
+/// Use `println!` only for the primary output of your program.
+///
+/// # Examples
+///
+/// ```
+/// println!("hello there!");
+/// println!("format {} arguments", "some");
+/// let local_variable = "some";
+/// println!("format {local_variable} arguments");
+/// ```
+#[macro_export]
+macro_rules! println {
+    ($fmt: literal $(, $($arg: tt)+)?) => {
+        $crate::console::print(format_args!(concat!($fmt, "\n") $(, $($arg)+)?));
+    }
+}
