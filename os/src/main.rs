e8bf30f31e204b1d9d2ef87552c79ff351769f1c
SARDONYX-sard <68905624+SARDONYX-sard@users.noreply.github.com>
2022-08-26 04:06:04 +0000

    refactor(os-main): implement code to clear bss

--- os/src/main.rs
+++ os/src/main.rs
@@ -1,7 +1,23 @@
-#![no_std]
 #![no_main]
+#![no_std]
 
 mod lang_items;
 
 use core::arch::global_asm;
 global_asm!(include_str!("entry.asm"));
+
+#[no_mangle]
+fn rust_main() -> ! {
+    loop {}
+}
+
+fn clear_bss() {
+    extern "C" {
+        fn sbss();
+        fn ebss();
+    }
+
+    // Dereference the memory address of sbss and ebss, and write 0
+    // like(*sbss = 0, *ebss = 0)
+    (sbss as usize..ebss as usize).for_each(|a| unsafe { (a as *mut u8).write_volatile(0) });
+}
