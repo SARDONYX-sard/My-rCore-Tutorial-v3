bd21b660600bcf24c317ef0da2c9ed002056552b
SARDONYX-sard <68905624+SARDONYX-sard@users.noreply.github.com>
2022-08-29 09:44:50 +0000

    reafactor(os): modify print info

--- os/src/lang_items.rs
+++ os/src/lang_items.rs
@@ -9,19 +9,19 @@ use crate::sbi::shutdown;
 ///
 /// ```
 /// panic!("Shutdown machine!");
-/// // >Panicked at src/main.rs:16 Shutdown machine!
+/// // >[kernel] Panicked at src/main.rs:16 Shutdown machine!
 /// ```
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
     if let Some(location) = info.location() {
         println!(
-            "Panicked at {}:{} {}",
+            "[kernel] Panicked at {}:{} {}",
             location.file(),
             location.line(),
             info.message().unwrap()
         );
     } else {
-        println!("Panicked: {}", info.message().unwrap())
+        println!("[kernel] Panicked: {}", info.message().unwrap());
     }
     shutdown()
 }
