ea0c3238b137ce8da572c29ae5398708f33dc8ac
SARDONYX-sard <68905624+SARDONYX-sard@users.noreply.github.com>
2022-09-01 06:24:22 +0000

    style(os-entry): change from 2tab to 4tab

--- os/src/entry.asm
+++ os/src/entry.asm
@@ -1,14 +1,14 @@
 # .text.entry is just a symbol for a segment,
 # and is just a name for placement in linker.ld
-.section .text.entry
-.globl _start
+    .section .text.entry
+    .globl _start
 _start:
-  la sp, boot_stack_top # load address dist, src_symbol: sp <- boot_stack_top
-  call rust_main        # This function is defined in main.rs
+    la sp, boot_stack_top # load address dist, src_symbol: sp <- boot_stack_top
+    call rust_main        # This function is defined in main.rs
 
-  .section .bss.stack
-  .globl boot_stack     # become global symbol. base stack
+    .section .bss.stack
+    .globl boot_stack     # become global symbol. base stack
 boot_stack:
-  .space 4096 * 16      # 64KiB
-  .globl boot_stack_top # become global symbol to label
+    .space 4096 * 16      # 64KiB
+    .globl boot_stack_top # become global symbol to label
 boot_stack_top:
