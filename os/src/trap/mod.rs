025970a3325fc472a37c6a1d428c8b3ed6e10fbf
SARDONYX-sard <68905624+SARDONYX-sard@users.noreply.github.com>
2022-09-01 06:27:35 +0000

    feat(os-trap): add exception

--- os/src/trap/mod.rs
+++ os/src/trap/mod.rs
@@ -14,16 +14,17 @@
 
 mod context;
 
+use crate::syscall::syscall;
+use crate::task::{exit_current_and_run_next, suspend_current_and_run_next};
+use crate::timer::set_next_trigger;
 use core::arch::global_asm;
 use riscv::register::{
-    scause::{self, Exception, Trap},
-    stval, stvec,
-    stvec::TrapMode,
+    scause::{self, Exception, Interrupt, Trap},
+    sie, stval,
+    stvec::{self, TrapMode},
 };
 
-pub use self::context::TrapContext;
-use crate::batch::run_next_app;
-use crate::syscall::syscall;
+pub use context::TrapContext;
 
 global_asm!(include_str!("trap.S"));
 
@@ -34,6 +35,13 @@ pub fn init() {
     }
 
     unsafe { stvec::write(__alltraps as usize, TrapMode::Direct) }
+}
+
+/// timer interrupt enabled
+pub fn enable_timer_interrupt() {
+    unsafe {
+        sie::set_stimer();
+    }
 }
 
 /// handle an interrupt, exception, or system call from user space
@@ -50,12 +58,16 @@ pub fn trap_handler(cx: &mut TrapContext) -> &mut TrapContext {
             cx.x[10] = syscall(cx.x[17], [cx.x[10], cx.x[11], cx.x[12]]) as usize;
         }
         Trap::Exception(Exception::StoreFault) | Trap::Exception(Exception::StorePageFault) => {
-            println!("[kernel] PageFault in application, kernel killed it.");
-            run_next_app();
+            println!("[kernel] PageFault in application, bad addr = {:#x}, bad instruction = {:#x}, kernel killed it.", stval, cx.sepc);
+            exit_current_and_run_next();
         }
         Trap::Exception(Exception::IllegalInstruction) => {
             println!("[kernel] IllegalInstruction in application, kernel killed it.");
-            run_next_app();
+            exit_current_and_run_next();
+        }
+        Trap::Interrupt(Interrupt::SupervisorTimer) => {
+            set_next_trigger();
+            suspend_current_and_run_next();
         }
         _ => {
             panic!(
