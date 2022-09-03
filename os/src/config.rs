a6cfe98e5a6ef9cb05fb7271ac90ff3bb9918389
SARDONYX-sard <68905624+SARDONYX-sard@users.noreply.github.com>
2022-09-03 19:53:10 +0000

    feat(os-config): add global variables for page
    - add `PAGE_SIZE_BITS`
    - add `PAGE_SIZE`

--- os/src/config.rs
+++ os/src/config.rs
@@ -1,11 +1,19 @@
 //! Constants used in rCore
 
+/// 4096byte == 4KiB
 pub const USER_STACK_SIZE: usize = 4096;
+/// 4096 * 2 = 8KiB
 pub const KERNEL_STACK_SIZE: usize = 4096 * 2;
-pub const KERNEL_HEAP_SIZE: usize = 0x30_0000; // 3MiB
+/// 0x300000byte == 3MiB
+pub const KERNEL_HEAP_SIZE: usize = 0x30_0000;
 pub const MAX_APP_NUM: usize = 4;
 pub const APP_BASE_ADDRESS: usize = 0x80400000;
 pub const APP_SIZE_LIMIT: usize = 0x20000;
+
+/// Bit width of intra-page offset
+pub const PAGE_SIZE_BITS: usize = 12;
+/// 4096byte == 4KiB
+pub const PAGE_SIZE: usize = 4096;
 
 /*
 #[cfg(feature = "board_k210")]
