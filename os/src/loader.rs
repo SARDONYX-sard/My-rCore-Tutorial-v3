52715e48b6606e2d032c6831cb01455d884e9cdb
SARDONYX-sard <68905624+SARDONYX-sard@users.noreply.github.com>
2022-09-05 14:09:52 +0000

    refactor(os-loader): change function
    from `init_app_cx` to `get_app_data`

--- os/src/loader.rs
+++ os/src/loader.rs
@@ -89,38 +89,21 @@ pub fn get_num_app() -> usize {
     unsafe { (_num_app as usize as *const usize).read_volatile() }
 }
 
-/// Load nth user app at
-/// [APP_BASE_ADDRESS + n * APP_SIZE_LIMIT, APP_BASE_ADDRESS + (n+1) * APP_SIZE_LIMIT).
-pub fn load_apps() {
+/// Based on the application number passed as an argument,
+/// obtain the ELF format executable data of the corresponding application.
+pub fn get_app_data(app_id: usize) -> &'static [u8] {
     extern "C" {
+        /// Get the number of applications linked to the kernel.
         fn _num_app();
     }
     let num_app_ptr = _num_app as usize as *const usize;
     let num_app = get_num_app();
     let app_start = unsafe { core::slice::from_raw_parts(num_app_ptr.add(1), num_app + 1) };
-    // clear i-cache first
+    assert!(app_id < num_app);
     unsafe {
-        asm!("fence.i");
+        core::slice::from_raw_parts(
+            app_start[app_id] as *const u8,
+            app_start[app_id + 1] - app_start[app_id],
+        )
     }
-    // load apps
-    for i in 0..num_app {
-        let base_i = get_base_i(i);
-        // clear region
-        (base_i..base_i + APP_SIZE_LIMIT)
-            .for_each(|addr| unsafe { (addr as *mut u8).write_volatile(0) });
-        // load app from data section to memory
-        let src = unsafe {
-            core::slice::from_raw_parts(app_start[i] as *const u8, app_start[i + 1] - app_start[i])
-        };
-        let dst = unsafe { core::slice::from_raw_parts_mut(base_i as *mut u8, src.len()) };
-        dst.copy_from_slice(src);
-    }
-}
-
-/// get app info with entry and sp and save `TrapContext` in kernel stack
-pub fn init_app_cx(app_id: usize) -> usize {
-    KERNEL_STACK[app_id].push_context(TrapContext::app_init_context(
-        get_base_i(app_id),
-        USER_STACK[app_id].get_sp(),
-    ))
 }
