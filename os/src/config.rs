c006047ea8d78b110f3ee1907ff2b922e40d7fe9
SARDONYX-sard <68905624+SARDONYX-sard@users.noreply.github.com>
2022-09-11 07:06:17 +0000

    chore(os-config): remove unnecessary code

--- os/src/config.rs
+++ os/src/config.rs
@@ -1,13 +1,12 @@
 //! Constants used in rCore
 
 /// 4096byte == 4KiB
-pub const USER_STACK_SIZE: usize = 4096;
+pub const USER_STACK_SIZE: usize = 4096 * 2;
 /// 4096 * 2 = 8KiB
 pub const KERNEL_STACK_SIZE: usize = 4096 * 2;
-/// 0x300000byte == 3MiB
-pub const KERNEL_HEAP_SIZE: usize = 0x30_0000;
-/// 0x80800000 = 2.008GiB, 0x800000 = 8MiB
-pub const MEMORY_END: usize = 0x80800000;
+/// 0x200000 byte == 2MiB
+pub const KERNEL_HEAP_SIZE: usize = 0x20_0000;
+
 /// 4096byte == 4KiB
 pub const PAGE_SIZE: usize = 0x1000;
 /// Bit width of intra-page offset
@@ -20,18 +19,5 @@ pub const TRAMPOLINE: usize = usize::MAX - PAGE_SIZE + 1;
 /// - TRAMPOLINE - PAGE_SIZE
 pub const TRAP_CONTEXT: usize = TRAMPOLINE - PAGE_SIZE;
 
-/// Return (bottom, top) of a kernel stack in kernel space.
-pub fn kernel_stack_position(app_id: usize) -> (usize, usize) {
-    let top = TRAMPOLINE - app_id * (KERNEL_STACK_SIZE + PAGE_SIZE);
-    let bottom = top - KERNEL_STACK_SIZE;
-    (bottom, top)
-}
-
-/*
-#[cfg(feature = "board_k210")]
-pub const CLOCK_FREQ: usize = 403000000 / 62;
 #[cfg(feature = "board_qemu")]
-pub const CLOCK_FREQ: usize = 12500000;
-*/
-#[cfg(feature = "board_qemu")]
-pub use crate::board::{CLOCK_FREQ, MMIO};
+pub use crate::board::{CLOCK_FREQ, MEMORY_END, MMIO};
