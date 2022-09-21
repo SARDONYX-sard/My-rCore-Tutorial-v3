e3ad35ac45a8dee41b000fa518140b01eec349c9
SARDONYX-sard <68905624+SARDONYX-sard@users.noreply.github.com>
2022-09-21 05:59:04 +0000

    refactor(easy-fs): organize import, docs

--- easy-fs/src/block_dev.rs
+++ easy-fs/src/block_dev.rs
@@ -1,9 +1,9 @@
 use core::any::Any;
-use core::marker::{Send, Sync};
-
+/// Trait for block devices
+/// which reads and writes data in the unit of blocks
 pub trait BlockDevice: Send + Sync + Any {
-    ///  Reads the block number `block_id` from disk to the buffer `buf` in memory.
+    ///Read data form block to buffer
     fn read_block(&self, block_id: usize, buf: &mut [u8]);
-    /// Writes the data in memory buffer `buf` to the block numbered by `block_id` on disk.
+    ///Write data from buffer to block
     fn write_block(&self, block_id: usize, buf: &[u8]);
 }
