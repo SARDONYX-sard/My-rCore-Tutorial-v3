3ecf4717376ecfd100df7b50eaa9ff0e3c37e75a
SARDONYX-sard <68905624+SARDONYX-sard@users.noreply.github.com>
2022-09-21 06:21:07 +0000

    feat(easy-fs): add `efs`

--- easy-fs/src/lib.rs
+++ easy-fs/src/lib.rs
@@ -1,15 +1,22 @@
+//!An easy file system isolated from the kernel
 #![no_std]
-#![no_main]
-
+#![deny(missing_docs)]
 extern crate alloc;
-
 mod bitmap;
 mod block_cache;
 mod block_dev;
+mod efs;
 mod layout;
-
+mod vfs;
 /// 1 sector == 512byte
 ///
-/// the default block size for the Linux Ext4 file system is 4096 bytes.
-/// easy-fs's implementation equates blocks and sectors to 512 bytes.
+/// The default block size for the Linux Ext4 file system is 4096 bytes.
+///
+/// `easy-fs`'s implementation equates blocks and sectors to 512 bytes.
 pub const BLOCK_SZ: usize = 512;
+use bitmap::Bitmap;
+use block_cache::{block_cache_sync_all, get_block_cache};
+pub use block_dev::BlockDevice;
+pub use efs::EasyFileSystem;
+use layout::*;
+pub use vfs::Inode;
