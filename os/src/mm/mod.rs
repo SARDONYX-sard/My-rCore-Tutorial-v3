f3d40052f2ec64d18404251a43484ffbefb1206a
SARDONYX-sard <68905624+SARDONYX-sard@users.noreply.github.com>
2022-09-25 08:27:52 +0000

    feat(mm): add public items in `mod.rs`

--- os/src/mm/mod.rs
+++ os/src/mm/mod.rs
@@ -12,11 +12,12 @@ mod heap_allocator;
 mod memory_set;
 mod page_table;
 
+pub use address::StepByOne;
+use address::VPNRange;
 pub use address::{PhysAddr, PhysPageNum, VirtAddr, VirtPageNum};
-use address::{StepByOne, VPNRange};
-pub use frame_allocator::{frame_alloc, FrameTracker};
+pub use frame_allocator::{frame_alloc, frame_dealloc, FrameTracker};
 pub use memory_set::remap_test;
-pub use memory_set::{MapPermission, MemorySet, KERNEL_SPACE};
+pub use memory_set::{kernel_token, MapPermission, MemorySet, KERNEL_SPACE};
 pub use page_table::{translated_byte_buffer, translated_refmut, translated_str, PageTableEntry};
 pub use page_table::{PTEFlags, PageTable, UserBuffer};
 
