7f4e397ea6851c310e128237db79f4ecab790453
SARDONYX-sard <68905624+SARDONYX-sard@users.noreply.github.com>
2022-09-25 09:53:15 +0000

    feat(os-fs): add `inode` structure

---
+++ os/src/fs/inode.rs
@@ -0,0 +1,71 @@
+//! `Arc<Inode>` -> `OSInodeInner`: In order to open files concurrently
+//! we need to wrap `Inode` into `Arc`,but `Mutex` in `Inode` prevents
+//! file systems from being accessed simultaneously
+//!
+//! `UPSafeCell<OSInodeInner>` -> `OSInode`: for static `ROOT_INODE`,we
+//! need to wrap `OSInodeInner` into `UPSafeCell`
+use super::File;
+use crate::sync::UPSafeCell;
+use alloc::sync::Arc;
+use easy_fs::Inode;
+
+pub struct OSInode {
+    /// Whether the file is allowed to be read by `sys_read` or not.
+    readable: bool,
+    /// Whether the file is allowed to be write by `sys_write` or not.
+    writable: bool,
+
+    inner: UPSafeCell<OSInodeInner>,
+}
+
+pub struct OSInodeInner {
+    ///
+    ///
+    /// The offset is maintained during `sys_read/write`.
+    offset: usize,
+    inode: Arc<Inode>,
+}
+
+impl OSInode {
+    pub fn new(readable: bool, writable: bool, inode: Arc<Inode>) -> Self {
+        Self {
+            readable,
+            writable,
+            inner: unsafe { UPSafeCell::new(OSInodeInner { offset: 0, inode }) },
+        }
+    }
+}
+
+impl File for OSInode {
+    fn readable(&self) -> bool {
+        self.readable
+    }
+
+    fn writable(&self) -> bool {
+        self.writable
+    }
+
+    fn read(&self, mut buf: crate::mm::UserBuffer) -> usize {
+        let mut inner = self.inner.exclusive_access();
+        let mut total_read_size = 0usize;
+        for slice in buf.buffers.iter_mut() {
+            let read_size = inner.inode.read_at(inner.offset, *slice);
+            assert_eq!(read_size, slice.len());
+            inner.offset += read_size;
+            total_read_size += read_size;
+        }
+        total_read_size
+    }
+
+    fn write(&self, buf: crate::mm::UserBuffer) -> usize {
+        let mut inner = self.inner.exclusive_access();
+        let mut total_write_size = 0usize;
+        for slice in buf.buffers.iter() {
+            let write_size = inner.inode.write_at(inner.offset, *slice);
+            assert_eq!(write_size, slice.len());
+            inner.offset += write_size;
+            total_write_size += write_size;
+        }
+        total_write_size
+    }
+}
--- os/src/fs/mod.rs
+++ os/src/fs/mod.rs
@@ -1,6 +1,15 @@
+mod inode;
+
 use crate::mm::UserBuffer;
 
+/// File trait
 pub trait File: Send + Sync {
+    /// If readable
+    fn readable(&self) -> bool;
+    /// If writable
+    fn writable(&self) -> bool;
+    /// Read file to `UserBuffer`
     fn read(&self, buf: UserBuffer) -> usize;
+    /// Write `UserBuffer` to file
     fn write(&self, buf: UserBuffer) -> usize;
 }
