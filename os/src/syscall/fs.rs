408b6474b144b75e54d42cb13d8436c0d2531845
SARDONYX-sard <68905624+SARDONYX-sard@users.noreply.github.com>
2022-09-25 17:14:31 +0000

    feat(os-syscall): implement `sys_open`,`sys_close`

--- os/src/syscall/fs.rs
+++ os/src/syscall/fs.rs
@@ -1,8 +1,9 @@
 //! File and filesystem-related syscalls
 
-use crate::mm::translated_byte_buffer;
+use crate::fs::{open_file, OpenFlags};
+use crate::mm::{translated_byte_buffer, translated_str};
 use crate::sbi::console_getchar;
-use crate::task::{current_user_token, suspend_current_and_run_next};
+use crate::task::{current_task, current_user_token, suspend_current_and_run_next};
 
 const FD_STDIN: usize = 0;
 const FD_STDOUT: usize = 1;
@@ -53,4 +54,71 @@ pub fn sys_read(fd: usize, buf: *const u8, len: usize) -> isize {
             panic!("Unsupported fd in sys_read!");
         }
     }
+}
+
+/// Opens a regular file and returns an accessible file descriptor.
+///
+/// # Parameters
+/// - `path`: Describe the filename of the file to be opened (for simplicity,
+/// the file system does not need to support directories, all files are placed in the root(`/`) directory).
+/// - `flags`: Describe the flags to be used when opening the file.
+///
+/// # Flags
+///
+/// | flags-bit |  permission  |                               Meaning                                     |
+/// |-----------|--------------|---------------------------------------------------------------------------|
+/// |-----------|--------------|---------------------------------------------------------------------------|
+/// |         0 |    read-only | it is opened in read-only mode `RDONLY`.                                  |
+/// |-----------|--------------|---------------------------------------------------------------------------|
+/// |  0(0x001) |   write-only | it is opened in write-only mode `WRONLY`.                                 |
+/// |-----------|--------------|---------------------------------------------------------------------------|
+/// |  1(0x002) | read & write | `RDWR` for both read and write.                                           |
+/// |-----------|--------------|---------------------------------------------------------------------------|
+/// |  9(0x200) |       create | `CREATE` of the file is allowed and should be created if it is not found; |
+/// |           |              | if it already exists, the file size should be set to zero.                |
+/// |-----------|--------------|---------------------------------------------------------------------------|
+/// | 10(0x400) |        trunc | it should be cleared and the size set back to zero,                       |
+/// |           |              | i.e. `TRUNC`, when opening the file.                                      |
+/// |-----------|--------------|---------------------------------------------------------------------------|
+///
+/// # Return
+/// Conditional branching.
+/// - if there is an error => -1
+/// - otherwise=> returns the file descriptor of the file normally.
+///               Possible error cause: the file does not exist.
+pub fn sys_open(path: *const u8, flags: u32) -> isize {
+    let task = current_task().unwrap();
+    let token = current_user_token();
+    let path = translated_str(token, path);
+    if let Some(inode) = open_file(path.as_str(), OpenFlags::from_bits(flags).unwrap()) {
+        let mut inner = task.inner_exclusive_access();
+        let fd = inner.alloc_fd();
+        inner.fd_table[fd] = Some(inode);
+        fd as isize
+    } else {
+        -1
+    }
+}
+
+/// The current process closes the file.
+///
+/// # Parameter
+/// - `fd`: File descriptor of the file to close.
+///
+/// # Return
+/// Conditional branching.
+/// - if the process terminated successfully => 0
+/// - otherwise => -1
+///   - Error cause: the file descriptor passed may not correspond to the file being opened.
+pub fn sys_close(fd: usize) -> isize {
+    let task = current_task().unwrap();
+    let mut inner = task.inner_exclusive_access();
+    if fd >= inner.fd_table.len() {
+        return -1;
+    }
+    if inner.fd_table[fd].is_none() {
+        return -1;
+    }
+    inner.fd_table[fd].take();
+    0
 }
