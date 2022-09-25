7f8c3b05266ed4fa90b216b770bba83cbb41c389
SARDONYX-sard <68905624+SARDONYX-sard@users.noreply.github.com>
2022-09-25 16:53:15 +0000

    feat(os-fs): add `Stdin` & `Stdout` structures

--- os/src/fs/mod.rs
+++ os/src/fs/mod.rs
@@ -1,4 +1,5 @@
 mod inode;
+mod stdio;
 
 use crate::mm::UserBuffer;
 
@@ -9,9 +10,16 @@ pub trait File: Send + Sync {
     /// If writable
     fn writable(&self) -> bool;
     /// Read file to `UserBuffer`
+    ///
+    /// # Return
+    /// Size of buffer read
     fn read(&self, buf: UserBuffer) -> usize;
     /// Write `UserBuffer` to file
+    ///
+    /// # Return
+    /// Size of written buffer
     fn write(&self, buf: UserBuffer) -> usize;
 }
 
 pub use inode::{list_apps, open_file, OSInode, OpenFlags};
+pub use stdio::{Stdin, Stdout};
