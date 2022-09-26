2e2e0bd3c3086aa8cc6a8528bd91383c5473e30b
SARDONYX-sard <68905624+SARDONYX-sard@users.noreply.github.com>
2022-09-26 05:46:06 +0000

    feat(os-boards): add RTC MMIO

--- os/src/boards/qemu.rs
+++ os/src/boards/qemu.rs
@@ -6,8 +6,10 @@ pub const CLOCK_FREQ: usize = 12500000;
 pub const MEMORY_END: usize = 0x81000000;
 
 /// Memory mapped I/O Address range (starting address, length)
+//- https://lists.gnu.org/archive/html/qemu-riscv/2019-11/msg00023.html
 pub const MMIO: &[(usize, usize)] = &[
-    (0x0010_1000, 0x00_1000), // Virtio Block in virt machine
+    (0x0010_0000, 0x00_2000), // VIRT_TEST/RTC  in virt machine
+    (0x1000_1000, 0x00_1000), // Virtio Block in virt machine
 ];
 
 pub type BlockDeviceImpl = crate::drivers::block::VirtIOBlock;
