c95f124f38c3f9bb8fb7d434487923a4398c846c
SARDONYX-sard <68905624+SARDONYX-sard@users.noreply.github.com>
2022-09-30 07:15:51 +0000

    feat(user-bin): support shell redirection

--- user/src/bin/user_shell.rs
+++ user/src/bin/user_shell.rs
@@ -15,69 +15,209 @@ const CR: u8 = 0x0du8;
 const DL: u8 = 0x7fu8;
 /// Keyboard keycode: BackSpace
 const BS: u8 = 0x08u8;
+const LINE_START: &str = "\x1b[32m|-[\x1b[0m/\x1b[32m]\n|-$ \x1b[0m";
 
 use alloc::string::String;
 use alloc::vec::Vec;
 use user_lib::console::getchar;
-use user_lib::{exec, fork, waitpid};
+use user_lib::{close, dup, exec, fork, open, pipe, waitpid, OpenFlags};
+
+#[derive(Debug)]
+struct ProcessArguments {
+    /// input side(e.g. "a < b" => 'b')
+    input: String,
+    /// output side(e.g. "a > b" => 'b')
+    output: String,
+    /// command line arguments(e.g. cat "filea" => \["cat", "filea"\])
+    args_copy: Vec<String>,
+    /// The address vector of command line arguments
+    args_addr: Vec<*const u8>,
+}
+
+impl ProcessArguments {
+    pub fn new(command: &str) -> Self {
+        // The &str of the args after the split is the subInterval of the line
+        // that contains not \0 at the end.
+        let args: Vec<_> = command.split(' ').collect();
+
+        // line is our input, and there is no not \0 in the middle.
+        // When we pass it to the kernel, we can only pass the first address of the string,
+        // so we must make sure it ends in \0.
+        let mut args_copy: Vec<String> = args
+            .iter()
+            .filter(|&arg| !arg.is_empty())
+            .map(|&arg| {
+                let mut string = String::new();
+                string.push_str(arg);
+                string.push('\0');
+                string
+            })
+            .collect();
+
+        // redirect input
+        let mut input = String::new();
+        if let Some((idx, _)) = args_copy
+            .iter()
+            .enumerate()
+            .find(|(_, arg)| arg.as_str() == "<\0")
+        {
+            input = args_copy[idx + 1].clone();
+            args_copy.drain(idx..=idx + 1);
+        }
+
+        // redirect output
+        let mut output = String::new();
+        if let Some((idx, _)) = args_copy
+            .iter()
+            .enumerate()
+            .find(|(_, arg)| arg.as_str() == ">\0")
+        {
+            output = args_copy[idx + 1].clone();
+            args_copy.drain(idx..=idx + 1);
+        }
+
+        let mut args_addr: Vec<*const u8> = args_copy.iter().map(|arg| arg.as_ptr()).collect();
+        // Each element of the args_addr vector represents the starting address of a command line argument string.
+        //
+        // It is the starting address of this vector that is passed to the kernel,
+        // so in order for the kernel to get the number of command line arguments,
+        // args_addr must end with a zero so that the kernel knows that the command line arguments have been taken
+        // when it sees them.
+        args_addr.push(core::ptr::null::<u8>());
+
+        Self {
+            input,
+            output,
+            args_copy,
+            args_addr,
+        }
+    }
+}
 
 #[no_mangle]
 pub fn main() -> i32 {
     println!("Rust user shell");
     let mut line: String = String::new();
-    print!(">> ");
+    print!("{}", LINE_START);
     loop {
         let c = getchar();
         match c {
             LF | CR => {
                 println!("");
                 if !line.is_empty() {
-                    // The &str of the args after the split is the subInterval of the line that contains not \0 at the end.
-                    let args: Vec<_> = line.as_str().split(' ').collect();
-                    let mut args_copy: Vec<String> = args
+                    let split: Vec<_> = line.as_str().split('|').collect();
+                    // cat filea | cat fileb
+                    //  => [filea, fileb]
+                    let process_arguments_list: Vec<_> = split
                         .iter()
-                        .map(|&arg| {
-                            let mut string = String::new();
-                            string.push_str(arg);
-                            string
-                        })
+                        .map(|&cmd| ProcessArguments::new(cmd))
                         .collect();
-                    // line is our input, and there is no not \0 in the middle.
-                    // When we pass it to the kernel, we can only pass the first address of the string,
-                    // so we must make sure it ends in \0.
-                    args_copy.iter_mut().for_each(|string| {
-                        // From there we use args_copy to copy the args string to the heap
-                        // and manually add the trailing \0.
-                        string.push('\0');
-                    });
-
-                    let mut args_addr: Vec<*const u8> =
-                        args_copy.iter().map(|arg| arg.as_ptr()).collect();
-                    // Each element of the args_addr vector represents the starting address of a command line argument string.
-                    //
-                    // It is the starting address of this vector that is passed to the kernel,
-                    // so in order for the kernel to get the number of command line arguments,
-                    // args_addr must end with a zero so that the kernel knows that the command line arguments have been taken
-                    // when it sees them.
-                    args_addr.push(core::ptr::null::<u8>());
-
-                    let pid = fork();
-                    if pid == 0 {
-                        // child process
-                        if exec(line.as_str(), args_addr.as_slice()) == -1 {
-                            println!("Error when executing!");
-                            return -4;
+                    let mut valid = true;
+                    for (i, process_args) in process_arguments_list.iter().enumerate() {
+                        if i == 0 {
+                            if !process_args.output.is_empty() {
+                                valid = false;
+                            }
+                        } else if i == process_arguments_list.len() - 1 {
+                            if !process_args.input.is_empty() {
+                                valid = false;
+                            }
+                        } else if !process_args.output.is_empty() || !process_args.input.is_empty()
+                        {
+                            valid = false;
                         }
-                        unreachable!();
+                    }
+                    if process_arguments_list.len() == 1 {
+                        valid = true;
+                    }
+                    if !valid {
+                        println!("Invalid command: Inputs/Outputs cannot be combined correctly!");
                     } else {
+                        // create pipes
+                        let mut pipes_fd: Vec<[usize; 2]> = Vec::new();
+                        if !process_arguments_list.is_empty() {
+                            for _ in 0..process_arguments_list.len() - 1 {
+                                let mut pipe_fd = [0usize; 2];
+                                pipe(&mut pipe_fd);
+                                pipes_fd.push(pipe_fd);
+                            }
+                        }
+                        let mut children: Vec<_> = Vec::new();
+                        for (i, process_argument) in process_arguments_list.iter().enumerate() {
+                            let pid = fork();
+                            if pid == 0 {
+                                let input = &process_argument.input;
+                                let output = &process_argument.output;
+                                let args_copy = &process_argument.args_copy;
+                                let args_addr = &process_argument.args_addr;
+                                // redirect input
+                                if !input.is_empty() {
+                                    let input_fd = open(input.as_str(), OpenFlags::RDONLY);
+                                    if input_fd == -1 {
+                                        println!("Error when opening file {}", input);
+                                        return -4;
+                                    }
+                                    let input_fd = input_fd as usize;
+                                    close(0);
+                                    assert_eq!(dup(input_fd), 0);
+                                    close(input_fd);
+                                }
+                                // redirect output
+                                if !output.is_empty() {
+                                    let output_fd = open(
+                                        output.as_str(),
+                                        OpenFlags::CREATE | OpenFlags::WRONLY,
+                                    );
+                                    if output_fd == -1 {
+                                        println!("Error when opening file {}", output);
+                                        return -4;
+                                    }
+                                    let output_fd = output_fd as usize;
+                                    close(1);
+                                    assert_eq!(dup(output_fd), 1);
+                                    close(output_fd);
+                                }
+                                // receive input from the previous process
+                                if i > 0 {
+                                    close(0);
+                                    let read_end = pipes_fd.get(i - 1).unwrap()[0];
+                                    assert_eq!(dup(read_end), 0);
+                                }
+                                // send output to the next process
+                                if i < process_arguments_list.len() - 1 {
+                                    close(1);
+                                    let write_end = pipes_fd.get(i).unwrap()[1];
+                                    assert_eq!(dup(write_end), 1);
+                                }
+                                // close all pipe ends inherited from the parent process
+                                for pipe_fd in pipes_fd.iter() {
+                                    close(pipe_fd[0]);
+                                    close(pipe_fd[1]);
+                                }
+                                // execute new application
+                                if exec(args_copy[0].as_str(), args_addr.as_slice()) == -1 {
+                                    println!("Error when executing!");
+                                    return -4;
+                                }
+                                unreachable!();
+                            } else {
+                                children.push(pid);
+                            }
+                        }
+                        for pipe_fd in pipes_fd.iter() {
+                            close(pipe_fd[0]);
+                            close(pipe_fd[1]);
+                        }
                         let mut exit_code: i32 = 0;
-                        let exit_pid = waitpid(pid as usize, &mut exit_code);
-                        assert_eq!(pid, exit_pid);
-                        println!("Shell: Process {} exited with code {}", pid, exit_code);
+                        for pid in children.into_iter() {
+                            let exit_pid = waitpid(pid as usize, &mut exit_code);
+                            assert_eq!(pid, exit_pid);
+                            //println!("Shell: Process {} exited with code {}", pid, exit_code);
+                        }
                     }
                     line.clear();
                 }
-                print!(">> ");
+                print!("{}", LINE_START);
             }
             BS | DL => {
                 if !line.is_empty() {
