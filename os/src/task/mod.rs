7c8ebb4af41fc20713761f47bc6d964691ceb2cb
SARDONYX-sard <68905624+SARDONYX-sard@users.noreply.github.com>
2022-11-15 18:50:50 +0000

    refactor(os-task): separate block task function

--- os/src/task/mod.rs
+++ os/src/task/mod.rs
@@ -61,13 +61,17 @@ pub fn suspend_current_and_run_next() {
     schedule(task_cx_ptr);
 }
 
-/// Stop the currently running task by setting its state to `Blocking` and switch to another task that is idle.
-pub fn block_current_and_run_next() {
+/// Stop the currently running task by setting its state to `Blocking`
+pub fn block_current_task() -> *mut TaskContext {
     let task = take_current_task().unwrap();
     let mut task_inner = task.inner_exclusive_access();
-    let task_cx_ptr = &mut task_inner.task_cx as *mut TaskContext;
     task_inner.task_status = TaskStatus::Blocking;
-    drop(task_inner);
+    &mut task_inner.task_cx as *mut TaskContext
+}
+
+/// Stop the currently running task by setting its state to `Blocking` and switch to another task that is idle.
+pub fn block_current_and_run_next() {
+    let task_cx_ptr = block_current_task();
     schedule(task_cx_ptr);
 }
 
