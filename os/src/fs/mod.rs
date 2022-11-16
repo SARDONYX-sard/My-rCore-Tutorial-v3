9e6b311d63fea1f152f4a25555ba24baa69671f4
SARDONYX-sard <68905624+SARDONYX-sard@users.noreply.github.com>
2022-11-16 07:47:09 +0000

    feat(os-fs): add reexport
    - `Pipe`
    - `ROOT_INODE`

--- os/src/fs/mod.rs
+++ os/src/fs/mod.rs
@@ -23,6 +23,6 @@ pub trait File: Send + Sync {
     fn write(&self, buf: UserBuffer) -> usize;
 }
 
-pub use inode::{list_apps, open_file, OSInode, OpenFlags};
-pub use pipe::make_pipe;
+pub use inode::{list_apps, open_file, OSInode, OpenFlags, ROOT_INODE};
+pub use pipe::{make_pipe, Pipe};
 pub use stdio::{Stdin, Stdout};
